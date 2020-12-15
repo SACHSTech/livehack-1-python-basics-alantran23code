ca0f5f1631878953a4004706cf367751da041888
Alan Tran <alan.tran23@ycdsbk12.ca>
2020-12-15 19:17:49 +0000

    LiveHack #1 completed

--- problem1.py
+++ problem1.py
@@ -1,1 +1,5 @@
-
+temperature_celsius = input("Enter temperature in Celsius: ")
+temperature_celsius = float(temperature_celsius)
+temperature_fahrenheit = temperature_celsius * 1.8 + 32
+temperature_fahrenheit = float(temperature_fahrenheit)
+print("The temperature in Fahrenheit is: " ,(temperature_fahrenheit))
--- problem2.py
+++ problem2.py
@@ -1,1 +1,7 @@
-
+number_of_students = 26
+number_of_chicken = input("Enter # of chicken: ")
+number_of_chicken = int(number_of_chicken)
+num_pieces_per_student = int(number_of_chicken / number_of_students)
+teachers_chicken = number_of_chicken - num_pieces_per_student * number_of_students
+print("The students get "+ str(num_pieces_per_student) +" pieces of chicken.")
+print("Mr. Fabroa gets "+ str(teachers_chicken) +" pieces of chicken.")
